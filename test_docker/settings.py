d7e47a84b540ff3494726dd451cc3433b296e7bb
Yannis Saliniere <yannis.saliniere@gmail.com>
2021-10-18 20:11:18 +0200

    version 0.0.2 modif .dockerignore and settings

--- test_docker/settings.py
+++ test_docker/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import environ
-import os
 from pathlib import Path
 
 env = environ.Env()
