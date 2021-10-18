7f4b4ec6a0bb445dbbaa786247b60e25b677ebfe
Yannis Saliniere <yannis.saliniere@gmail.com>
2021-10-18 23:53:55 +0200

    version 0.0.3 modif settings

--- test_docker/settings.py
+++ test_docker/settings.py
@@ -18,8 +18,6 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print("BASE_DIR", BASE_DIR)
-
 # Take environment variables from .env file
 environ.Env.read_env()
 
